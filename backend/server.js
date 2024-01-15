13259ee3bc819482bb57b964a356a088c78212f1
Nathaniel Torres <it.nathaniel.m.torres@gmail.com>
2024-01-15 09:19:15 +0800

    Serving react vite build app. [1]

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT
+const path = require('path')
 const cors = require('cors')
 
 app.use(cors())
